0cc25aafbdb8ca7d9459c289bfccf2af4fde1052
samiya <64473932+samiya12-hash@users.noreply.github.com>
2020-12-25 19:42:07 +0600

    Add files via upload

---
+++ templates.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define loop(i,n)for(i=0;i<n;i++)
+#define INF 1000000000
+
+
+using ll = long long int;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+
+typedef vector<int> vi;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+bool sortsec(const pair<int,int> &a,const pair<int,int> &b)
+{
+    if(a.second==b.second)return a.first>b.first;
+    else return (a.second>b.second);
+}
+
+
+
+#define eb emplace_back
+#define mp make_pair
+#define pb push_back
+#define MAX 100000
+#define MOD 1000000007
+#define endl '\n'
