a4c5c8f676d788fe288c1219d49f6b60bf437cb7
samiya <64473932+samiya12-hash@users.noreply.github.com>
2021-03-06 20:21:54 +0600

    Add files via upload

---
+++ constant&macro.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define sum 0;
+#define show(sum) (cout<<sum<<endl;)
+//#define MIN -1<<endl;
+//#define PI 2 * acos(0)<<endl;
+#define DOUBLE(a) {\
+       for(int i=1;i<=a;i++)sum+i;\
+}
+using namespace std;
+int main()
+{
+    int a=3;
+    //cout<<MIN
+    // cout<<PI
+    //cout<<DOUBLE(a);
+    DOUBLE(a);
+    cout<<sum<<endl;
+    return 0;
+}
