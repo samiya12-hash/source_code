0cc79d801098a888d0cf77e120276c214afaa4b3
samiya <64473932+samiya12-hash@users.noreply.github.com>
2022-05-26 23:35:06 +0600

    Update templates.cpp

--- templates.cpp
+++ templates.cpp
@@ -1,33 +1,80 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 
-#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define loop(i,n)for(i=0;i<n;i++)
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define loop(i, n) for (i = 0; i < n; i++)
 #define INF 1000000000
 
-
 using ll = long long int;
-typedef pair<int,int> pii;
+typedef pair<ll, ll> pii;
 typedef vector<pii> vii;
 
-typedef vector<int> vi;
-typedef tree<int,null_type,less<int>,rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
+typedef vector<ll> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    indexed_set;
 
-bool sortsec(const pair<int,int> &a,const pair<int,int> &b)
+bool sortsec(const pair<int, int> &a, const pair<int, int> &b)
 {
-    if(a.second==b.second)return a.first>b.first;
-    else return (a.second>b.second);
+    if (a.second == b.second)
+        return a.first > b.first;
+    else
+        return (a.second > b.second);
 }
-
-
 
 #define eb emplace_back
 #define mp make_pair
 #define pb push_back
-#define MAX 100000
+#define MAX 10000
 #define MOD 1000000007
 #define endl '\n'
+ll dx[4] = {-1, 1, 0, 0};
+ll dy[4] = {0, 0, 1, -1};
+ll lowbit(ll x){
+    return x&(-x);
+}
+
+
+ll binpow(ll a, ll b) {
+    if (b == 0)
+        return 1;
+    ll res = binpow(a, b / 2)%MOD;
+    if (b % 2)
+        return (res * res * a)%MOD;
+    else
+        return (res * res)%MOD;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    freopen("problemname.in", "r", stdin);
+    freopen("problemname.out", "w", stdout);
+*/
+
+
+
+
+int main()
+{
+    IOS;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+}
