53bd0a05c3709d1ce254448ece1685be1dd77af0
samiya <64473932+samiya12-hash@users.noreply.github.com>
2025-05-01 19:29:11 +0600

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -33,3 +33,49 @@ int main()
     cout<<endl<<endl;
     for(int i=1;i<=node;i++)cout<<visited[i]<<endl;
 }
+/*
+void dfs(map<ll,vector<ll>>&ma,map<ll,ll>&flag,ll &node,map<ll,ll>&visited,ll &ans)
+{
+    //cout<<node<<" "<<ans<<endl;
+    visited[node] = 1;
+    ans = max(ans,node);
+    if(flag[node]==0)return;
+    for(auto x:ma[node])
+    {
+        ll k = x+node;
+        if(visited[k]==0)dfs(ma,flag,k,visited,ans);
+    }
+}
+
+
+int main()
+{
+    IOS;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;cin>>t;
+    while(t--){
+        ll n;cin>>n;
+        vi v(n);
+        ll ans = 0;
+        for(auto &x:v)cin>>x;
+        map<ll,vector<ll>>ma;
+        map<ll,ll>flag;
+        for(ll i = 1;i<n;i++)
+        {
+            ll idx = v[i]+i;
+            ma[idx].pb(i);
+            flag[idx] = 1;
+        }
+        // for(auto x:ma)
+        // {
+        //     // cout<<x.first<<"- ";
+        //     for(auto y:x.second)cout<<y<<" ";
+        //     cout<<endl;
+        // }
+        map<ll,ll>visited;
+        dfs(ma,flag,n,visited,ans);
+        cout<<ans<<endl;
+    }
+   // https://codeforces.com/contest/2027/problem/C
+}*/
